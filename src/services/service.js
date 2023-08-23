6c517d6a70e09e3d50e97ee7233233025d287a03
nirsgv <nirsegevmail@gmail.com>
2023-08-23 17:25:29 +0300

    path to resolve html

--- src/services/service.js
+++ src/services/service.js
@@ -1,14 +1,13 @@
-/* eslint-disable no-debugger */
 import axios from "axios";
 
 const axi = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: "https://localhost:8000/api/",
   timeout: 4000,
   headers: { "X-Requested-With": "XMLHttpRequest" },
 });
 
 const getTrack = async ({ id }) => {
-  const { data } = await axi.get(`http://localhost:8000/api/track/${id}`);
+  const { data } = await axi.get(`https://localhost:8000/api/track/${id}`);
   console.log(data);
   // await new Promise(resolve => setTimeout(resolve, 1000));
   return data;
